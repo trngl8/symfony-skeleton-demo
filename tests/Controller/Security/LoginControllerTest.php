189d6f7c3528f044f22aecfe5c24cefae6aec3d4
Romаn Mоvchan <movchan@gmail.com>
2024-12-12 15:33:53 +0200

    Register, close #285

--- tests/Controller/Security/LoginControllerTest.php
+++ tests/Controller/Security/LoginControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Tests\Controller;
+namespace App\Tests\Controller\Security;
 
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
--- tests/Controller/Security/ResetPasswordControllerTest.php
+++ tests/Controller/Security/ResetPasswordControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Tests\Controller;
+namespace App\Tests\Controller\Security;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
