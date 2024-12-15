5ffaccdb5184e476d9dde7ff70cf46accdc4eff7
mvchn <movchan@gmail.com>
2024-12-15 16:46:45 +0200

    Verify

--- src/Controller/Security/UserVerificationController.php
+++ src/Controller/Security/UserVerificationController.php
@@ -2,29 +2,88 @@
 
 namespace App\Controller\Security;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\UserVerifyFormType;
 use App\Model\UserVerify;
+use SymfonyCasts\Bundle\ResetPassword\Exception\ResetPasswordExceptionInterface;
+use SymfonyCasts\Bundle\ResetPassword\ResetPasswordHelperInterface;
 
 class UserVerificationController extends AbstractController
 {
+    public function __construct(
+        private readonly MailerInterface $mailer,
+        private readonly EntityManagerInterface $em,
+        private readonly ResetPasswordHelperInterface $resetPasswordHelper,
+        private readonly string $adminEmail,
+        private readonly string $appName
+    )
+    {
+    }
+
     #[Route('/user/verify', name: 'app_user_verify')]
-    public function userVerify(Request $request): Response
+    public function verify(Request $request): Response
     {
         $userVerify = new UserVerify();
         $form = $this->createForm(UserVerifyFormType::class, $userVerify);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->addFlash('success', 'notice.check_code');
-            return $this->redirectToRoute('default');
+            $user = $this->em->getRepository(User::class)->findOneBy(['username' => $userVerify->email]);
+
+            if ($user) {
+                try {
+                    $verifyToken = $this->resetPasswordHelper->generateResetToken($user);
+                } catch (ResetPasswordExceptionInterface $e) {
+                    $this->addFlash('warning', $e->getReason());
+                    return $this->redirectToRoute('app_user_verify');
+                }
+
+                $email = (new TemplatedEmail())
+                    ->from(new Address($this->adminEmail, $this->appName))
+                    ->to($user->getUsername())
+                    ->subject('Your account verify link')
+                    ->htmlTemplate('email/verify.html.twig')
+                    ->context([
+                        'verifyToken' => $verifyToken,
+                    ])
+                ;
+
+                $this->mailer->send($email);
+            }
+
+            // TODO: log user
+            $this->addFlash('success', 'notice.check_email');
+            return $this->redirectToRoute('app_check_email');
         }
 
         return $this->render('themes/default/user_verify.html.twig', [
             'verifyForm' => $form->createView(),
         ]);
+    }
+
+    #[Route('/user/confirm', name: 'app_user_confirm')]
+    public function confirm(Request $request): Response
+    {
+        $token = $request->query->get('token');
+        $user = $this->resetPasswordHelper->validateTokenAndFetchUser($token);
+
+        if (!$user) {
+            $this->addFlash('error', 'notice.invalid_token');
+            return $this->redirectToRoute('app_user_verify');
+        }
+
+        $user->setVerified(true);
+        $this->em->flush();
+
+        $this->addFlash('success', 'notice.account_verified');
+        return $this->redirectToRoute('default');
     }
 }
